9cd903832003746b6b47488a84d64aa44dddd9a4
caio-maximiano <53972502+caio-maximiano@users.noreply.github.com>
2024-03-30 10:33:05 -0300

    Update Experience.py

--- app/pages/Experience.py
+++ app/pages/Experience.py
@@ -8,21 +8,21 @@ def projects_page():
         st.markdown("""
             **January 2023 - Current**
             - Drove the creation and management of batch and near real-time telemetry data pipelines as a Data Engineer specializing in precision agriculture at CNH Industrial. These pipelines catered to end-users and involved intricate transformations and aggregations across different layers within the lakehouse architecture.
-- Collaborated on internal analytics initiatives to improve machinery performance through the analysis of telemetry data. Leveraged data science principles to derive valuable insights and contribute to ongoing enhancements.
+            - Collaborated on internal analytics initiatives to improve machinery performance through the analysis of telemetry data. Leveraged data science principles to derive valuable insights and contribute to ongoing enhancements.
             """)
 
         st.header('Big Data Engineer at IBM')
         st.markdown("""
             **December 2021 - January 2023**
             - Played a key role in a machine learning project focused on reducing customer churn as a Data Engineer in the telecommunications industry at IBM. Responsibilities included creating tables that fueled the recommendation engine maintained by the data science team, ultimately optimizing customer recommendations.
-- Acquired hands-on experience with a diverse range of technologies, including Cloudera Data Platform, Azure Data Factory, Azure Data Lake Storage, Databricks, HDFS, Informatica Cloud, and Oracle Database, demonstrating versatility in managing data infrastructure.
+            - Acquired hands-on experience with a diverse range of technologies, including Cloudera Data Platform, Azure Data Factory, Azure Data Lake Storage, Databricks, HDFS, Informatica Cloud, and Oracle Database, demonstrating versatility in managing data infrastructure.
             """)
         
         st.header('Data Analyst at MG Info')
         st.markdown("""
             **August 2021 - December 2021**
             - Contributed as a Data Analyst to a global mining project at MG Info, collaborating closely with teams in South Africa and India. Played a key role in testing the data pipeline developed by the Indian team, ensuring its seamless integration and validation for systems destined for Azure Data Lake Storage.
-- Utilized Azure Databricks to meticulously verify data flow, model relationships, and maintain the accuracy of dashboards in alignment with the source system data. This involved active participation in cross-functional efforts to uphold data quality and consistency.
+            - Utilized Azure Databricks to meticulously verify data flow, model relationships, and maintain the accuracy of dashboards in alignment with the source system data. This involved active participation in cross-functional efforts to uphold data quality and consistency.
             """)
 
     with tab2:
